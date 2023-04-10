0f4d093d0610932efa98d646ea9da3349d24d4c5
James Aguirre <jamesaguirre98@gmail.com>
2023-04-10 21:59:44 +0000

    js event listeners

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function (event) {
+  const jSon = JSON.stringify(data);
+  this.localStorage.setItem('javascript-local-storage', jSon);
+}
+);
+
+if (localStorage !== null) {
+  data = JSON.parse('javascript-local-storage');
+}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+const $image = document.getElementById('img');
+const $photo = document.getElementById('img-placeholder');
+$image.addEventListener('input', function (event) {
+  $photo.setAttribute('src', event.target.value);
+}
+);
 
 const $form = document.getElementById('form');
 
 document.addEventListener('submit', function (event) {
   event.preventDefault();
   const messageData = {};
+  messageData.entryId = data.nextEntryId;
+  data.nextEntryId++;
   messageData.title = $form.title.value;
   messageData.img = $form.img.value;
   messageData.notes = $form.notes.value;
+  data.entries.push(messageData);
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 }
 );
