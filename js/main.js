7b346d3cf82288d069e5d0e7251762978fd46f55
James Aguirre <jamesaguirre98@gmail.com>
2023-04-14 01:53:36 +0000

    viewswap after edit is clearing properly

--- js/main.js
+++ js/main.js
@@ -29,23 +29,25 @@ $form.addEventListener('submit', function (event) {
     $form.reset();
     // updated code below
     $list.prepend(renderEntry(messageData));
+    $form.reset();
     viewSwap('entries');
     toggleNoEntries();
 
-    // 3rd edit below
-  } else if (data.editing !== null) {
-    messageData.entryId = data.editing.entryId;
-    messageData.title = event.target.elements.title.value;
-    messageData.img = event.target.elements.img.value;
-    messageData.notes = event.target.elements.notes.value;
-    const dataEntryIndex = data.entries.length - data.editing.entryId;
-    data.entries[dataEntryIndex] = messageData;
-    data.entries.splice(dataEntryIndex, 1, messageData);
+    // 3rd edit below, this is to fill out form template save edit information on submit
+  } else {
+    data.editing.title = event.target.elements.title.value;
+    data.editing.img = event.target.elements.img.value;
+    data.editing.notes = event.target.elements.notes.value;
+
+    for (let i = 0; i < $list.childNodes.length; i++) {
+      if ($list.childNodes[i].tagName === 'LI' && $list.childNodes[i].dataset.entryId * 1 === data.editing.entryId) {
+        $list.childNodes[i].replaceWith(renderEntry(data.editing));
+      }
+    }
+    $form.reset();
+    viewSwap('entries');
     $formHeader.textContent = 'New entry';
     data.editing = null;
-    viewSwap('entries');
-    location.reload();
-    toggleNoEntries();
   }
 }
 );
@@ -55,7 +57,7 @@ function renderEntry(entry) {
   $li.setAttribute('data-entry-id', entry.entryId);
 
   const $row = document.createElement('div');
-  $row.className = 'row';
+  $row.className = 'row-entries';
   $li.appendChild($row);
 
   const $columnHalf = document.createElement('div');
@@ -110,6 +112,8 @@ $anchorTag.addEventListener('click', function () {
 
 $anchorTagTwo.addEventListener('click', function () {
   viewSwap('entry-form');
+  const $notes = document.querySelector('#notes');
+  $notes.innerHTML = '';
 }
 );
 
@@ -125,9 +129,11 @@ function viewSwap(string) {
   data.view = string;
 }
 
+// This is to swap pages if icon is clicked
 $list.addEventListener('click', function () {
   if (event.target.tagName === 'I') {
     viewSwap('entry-form');
+    // in quotes + brackets to grab the nearest element with data-entry-id attribute
     let entryId = event.target.closest('[data-entry-id]').dataset.entryId;
     entryId = entryId * 1;
     for (let i = 0; i < data.entries.length; i++) {
@@ -140,10 +146,9 @@ $list.addEventListener('click', function () {
   const $img = document.querySelector('#img');
   const $notes = document.querySelector('#notes');
   const $headerNewEntry = document.querySelector('.entry-header');
-
   $headerNewEntry.innerHTML = 'Edit Entry';
   $title.value = data.editing.title;
   $img.value = data.editing.img;
-  $notes.textContent = data.editing.notes;
+  $notes.innerHTML = data.editing.notes;
 }
 );
